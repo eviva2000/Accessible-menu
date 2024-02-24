5891cec9d0ae1281bead3523fac0ac819104d307
eviva2000 <arjomandielaheh@gamin.com>
2024-02-24 20:29:43 +0100

    got the initial status of the menu up and running

---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import { Menu, Item } from "./Menu";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement!);
+
+root.render(
+  <StrictMode>
+    <Menu
+      renderTrigger={(props) => <button {...props}>Actions</button>}
+      onAction={alert}
+      shouldFlip={true}
+    >
+      <Item key="copy">Copy application</Item>
+      <Item key="rename">Rename application</Item>
+      <Item key="move" title="Move to">
+        <Item key="move-to-shared">Shared</Item>
+        <Item key="move-to-desktop">Desktop</Item>
+        <Item key="move-to-favorite">Favorite</Item>
+        <Item key="move-to-favorited">Fkavorite</Item>
+      </Item>
+      <Item key="delete">Delete application</Item>
+    </Menu>
+  </StrictMode>
+);
